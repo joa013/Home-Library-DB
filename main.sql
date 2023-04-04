0ce2afb4e5697d7ec7a5265858a04aa124a93ddc
Joanna Wachowicz <93839746+joa013@users.noreply.github.com>
2023-04-04 17:02:36 +0100

    Update main.sql
    Unified the naming convention between primary and foreigh keys

--- main.sql
+++ main.sql
@@ -1,16 +1,16 @@
 CREATE TABLE "Author"(
-  "author_id" INTEGER NOT NULL, 
+  "id" INTEGER NOT NULL, 
   "f_name" TEXT NOT NULL, 
   "l_name" TEXT NOT NULL, 
   PRIMARY KEY("author_id" AUTOINCREMENT)
   );
 
 CREATE TABLE "Book" (
-	"book_id"	INTEGER NOT NULL,
+	"id"	INTEGER NOT NULL,
 	"book_title"	TEXT NOT NULL,
 	"pub_year"	INTEGER,
-	"cat_id"	INTEGER,
-	"lang_id"	INTEGER,
+	"category_id"	INTEGER,
+	"language_id"	INTEGER,
 	"publisher_id"	INTEGER,
 	PRIMARY KEY("book_id" AUTOINCREMENT)
 );
@@ -22,19 +22,19 @@ CREATE TABLE "Book_Author" (
 );
 
 CREATE TABLE "Category" (
-	"cat_id"	INTEGER NOT NULL,
+	"id"	INTEGER NOT NULL,
 	"cat_name"	TEXT NOT NULL,
 	PRIMARY KEY("cat_id" AUTOINCREMENT)
 );
 
 CREATE TABLE "Language" (
-	"lang_id"	INTEGER NOT NULL,
+	"id"	INTEGER NOT NULL,
 	"language"	TEXT,
 	PRIMARY KEY("lang_id" AUTOINCREMENT)
 );
 
 CREATE TABLE "Loan" (
-	"loan_id"	INTEGER NOT NULL,
+	"id"	INTEGER NOT NULL,
 	"book_id"	INTEGER NOT NULL,
 	"member_id"	INTEGER NOT NULL,
 	"loan_date"	TEXT NOT NULL,
@@ -43,7 +43,7 @@ CREATE TABLE "Loan" (
 );
 
 CREATE TABLE "Member" (
-	"member_id"	INTEGER NOT NULL,
+	"id"	INTEGER NOT NULL,
 	"f_name"	TEXT NOT NULL,
 	"l_name"	TEXT NOT NULL,
 	"email"	TEXT,
@@ -51,13 +51,13 @@ CREATE TABLE "Member" (
 );
 
 CREATE TABLE "Publisher" (
-	"pub_id"	INTEGER NOT NULL,
+	"id"	INTEGER NOT NULL,
 	"pub_name"	TEXT NOT NULL,
 	PRIMARY KEY("pub_id")
 );
 
 CREATE TABLE "Shelf" (
-	"shelf_id"	INTEGER NOT NULL,
+	"id"	INTEGER NOT NULL,
 	"shelf"	TEXT NOT NULL,
 	PRIMARY KEY("shelf_id" AUTOINCREMENT)
 );
